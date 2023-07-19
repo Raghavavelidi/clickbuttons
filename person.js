04952e488fb06af62d044fb1d8dd53d54f20f8b0
Raghavavelidi <135940607+Raghavavelidi@users.noreply.github.com>
2023-07-19 18:43:43 +0530

    Add files via upload

---
+++ person.js
@@ -0,0 +1,10 @@
+function farmer(){
+    document.getElementById("globe").src="https://images.hindustantimes.com/rf/image_size_960x540/HT/p2/2020/12/19/Pictures/_c890ad0e-417b-11eb-a3f2-c7d95fc89ddb.jpg"
+}
+function officer(){
+    document.getElementById("globe").src="https://static.pib.gov.in/WriteReadData/userfiles/image/image001WHOB.jpg"
+}
+function business(){
+    document.getElementById("globe").src="https://png.pngtree.com/background/20210710/original/pngtree-creative-synthetic-businessman-and-city-picture-image_1062317.jpg"
+}
+
